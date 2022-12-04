6754d45876780ec2bda58aa49692f7e19c5c2efa
Hannahzoebr <115719428+Hannahzoebr@users.noreply.github.com>
2022-12-04 22:11:53 +0100

    Aufgabe_8

--- Aufgabe_8/script.js
+++ Aufgabe_8/script.js
@@ -1,1 +1,80 @@
-//# sourceMappingURL=script.js.map
+const button1 = document.getElementById("sound-1");
+
+button1.onclick = function() {
+  const audio = document.getElementById("clicksound1");
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const button2 = document.getElementById("sound-2");
+
+button2.onclick = function() {
+  const audio = document.getElementById("clicksound2");
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const button3 = document.getElementById("sound-3");
+
+button3.onclick = function() {
+  const audio = document.getElementById("clicksound3");
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const button4 = document.getElementById("sound-4");
+
+button4.onclick = function() {
+  const audio = document.getElementById("clicksound4");
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const button5 = document.getElementById("sound-5");
+
+button5.onclick = function() {
+  const audio = document.getElementById("clicksound5");
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const button6 = document.getElementById("sound-6");
+
+button6.onclick = function() {
+  const audio = document.getElementById("clicksound6");
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const button7 = document.getElementById("sound-7");
+
+button7.onclick = function() {
+  const audio = document.getElementById("clicksound7");
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const button8 = document.getElementById("sound-8");
+
+button8.onclick = function() {
+  const audio = document.getElementById("clicksound8");
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const button9 = document.getElementById("sound-9");
+
+button9.onclick = function() {
+  const audio = document.getElementById("clicksound9");
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+};
