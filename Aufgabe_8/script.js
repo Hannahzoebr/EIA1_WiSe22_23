1f81c75befad8f057d6a5c6a3541692bcd5d6893
Hannahzoebr <115719428+Hannahzoebr@users.noreply.github.com>
2022-12-04 22:13:20 +0100

    unverstanden

--- Aufgabe_8/script.js
+++ Aufgabe_8/script.js
@@ -1,80 +1,99 @@
+
 const button1 = document.getElementById("sound-1");
 
-button1.onclick = function() {
-  const audio = document.getElementById("clicksound1");
-  audio.pause();
-  audio.currentTime = 0;
-  audio.play();
+button1.onclick = function () {
+    const audio = document.getElementById("clicksound1");
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 };
+
 
 const button2 = document.getElementById("sound-2");
 
-button2.onclick = function() {
-  const audio = document.getElementById("clicksound2");
-  audio.pause();
-  audio.currentTime = 0;
-  audio.play();
+button2.onclick = function () {
+    const audio = document.getElementById("clicksound2");
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 };
+
 
 const button3 = document.getElementById("sound-3");
 
-button3.onclick = function() {
-  const audio = document.getElementById("clicksound3");
-  audio.pause();
-  audio.currentTime = 0;
-  audio.play();
+button3.onclick = function () {
+    const audio = document.getElementById("clicksound3");
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 };
+
 
 const button4 = document.getElementById("sound-4");
 
-button4.onclick = function() {
-  const audio = document.getElementById("clicksound4");
-  audio.pause();
-  audio.currentTime = 0;
-  audio.play();
+button4.onclick = function () {
+    const audio = document.getElementById("clicksound4");
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 };
+
 
 const button5 = document.getElementById("sound-5");
 
-button5.onclick = function() {
-  const audio = document.getElementById("clicksound5");
-  audio.pause();
-  audio.currentTime = 0;
-  audio.play();
+button5.onclick = function () {
+    const audio = document.getElementById("clicksound5");
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 };
+
 
 const button6 = document.getElementById("sound-6");
 
-button6.onclick = function() {
-  const audio = document.getElementById("clicksound6");
-  audio.pause();
-  audio.currentTime = 0;
-  audio.play();
+button6.onclick = function () {
+    const audio = document.getElementById("clicksound6");
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 };
+
 
 const button7 = document.getElementById("sound-7");
 
-button7.onclick = function() {
-  const audio = document.getElementById("clicksound7");
-  audio.pause();
-  audio.currentTime = 0;
-  audio.play();
+button7.onclick = function () {
+    const audio = document.getElementById("clicksound7");
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 };
+
 
 const button8 = document.getElementById("sound-8");
 
-button8.onclick = function() {
-  const audio = document.getElementById("clicksound8");
-  audio.pause();
-  audio.currentTime = 0;
-  audio.play();
+button8.onclick = function () {
+    const audio = document.getElementById("clicksound8");
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 };
+
 
 const button9 = document.getElementById("sound-9");
 
-button9.onclick = function() {
-  const audio = document.getElementById("clicksound9");
-  audio.pause();
-  audio.currentTime = 0;
-  audio.play();
+button9.onclick = function () {
+    const audio = document.getElementById("clicksound9");
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+};
+
+const button = document.getElsementById("random_play");
+
+button.onclick = function () {
+    const audio = document.getElementById("clicksound");
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+    Array.audio;
 };
